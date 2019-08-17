2efcfb3f7bc4151852e10333441ad779f7ed4433
gugameiru <gugameiru@gmail.com>
2019-08-17 21:34:37 +0300

    Day_4

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import getData from './modules/getData';
+import renderCards from './modules/renderCards';
+import renderCatalog from './modules/renderCatalog';
+import toggleCheckbox from './modules/toggleCheckbox';
+import toggleCart from './modules/toggleCart';
+import addCart from './modules/addCart';
+import actionPage from './modules/actionPage';
+import search from './modules/search';
+
+
+
+(async function() {
+  const db = await getData();
+  renderCards(db);
+  renderCatalog();
+  toggleCheckbox();
+  toggleCart();
+  addCart();
+  actionPage();
+  search();
+}());
